398de9082748442cbffc8ae170b60f76431183b7
EquipeWeb02 <equipeweb02@Mac-Filipe.local>
2023-01-02 18:03:19 -0200

    finalizado

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import Top from "../assets/top-image.png";
 import Bottom from "../assets/bottom-image.png";
 import Rocket from "../assets/rocket.png";
@@ -7,17 +7,45 @@ import { Modal } from "./Modal";
 
 export function Count() {
   const [isOpen, setIsOpen] = useState(false);
-  const [segundos, setSegundos] = useState(10);
-  const [minutos, setMinutos] = useState(2);
+  const [segundos, setSegundos] = useState(5);
+  const [minutos, setMinutos] = useState(1);
   const [horas, setHoras] = useState(1);
-  const [dias, setDias] = useState(1);
-  // React.useEffect(() => {
-  //   setInterval(() => setSegundos((cont) => cont - 1), 1000);
-  // }, []);
+  const [dias, setDias] = useState(0);
 
-  if (segundos === 0) {
-    setMinutos(minutos - 1);
-  }
+  const timer = () => {
+    setTimeout(() => {
+      setSegundos((cont) => cont - 1);
+
+      if (dias === -1) {
+        setDias(0);
+      }
+      if (minutos === -1) {
+        setMinutos(0);
+      }
+      if (horas === -1) {
+        setHoras(0);
+      }
+
+      if (horas === 0 && dias > 0) {
+        setDias((cont) => cont - 1);
+        setHoras(23);
+      }
+
+      if (minutos === 0 && horas > 0) {
+        setHoras((count) => count - 1);
+        setMinutos(59);
+      }
+
+      if (segundos === 0) {
+        setMinutos((count) => count - 1);
+        setSegundos(59);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    timer();
+  }, [segundos]);
 
   return (
     <div className="container">
@@ -29,16 +57,39 @@ export function Count() {
           <div className="container3">
             <h1 className="titulo">Pronto para lançar em...</h1>
             <h1 className="number">
-              {dias}:{horas}:{minutos}:{segundos}
+              {dias.toLocaleString("pt-br", {
+                minimumIntegerDigits: 2,
+                useGrouping: false,
+              })}
+              :
+              {horas.toLocaleString("pt-br", {
+                minimumIntegerDigits: 2,
+                useGrouping: false,
+              })}
+              :
+              {minutos.toLocaleString("pt-br", {
+                minimumIntegerDigits: 2,
+                useGrouping: false,
+              })}
+              :
+              {segundos.toLocaleString("pt-br", {
+                minimumIntegerDigits: 2,
+                useGrouping: false,
+              })}
             </h1>
-            <p> Dias {"     "} Horas Minutos Segundos</p>
+            <div className="tempo">
+              <p> Dias </p>
+              <p> Horas </p>
+              <p>Minutos</p>
+              <p>Segundos</p>
+            </div>
             <p>inscreva-se para saber mais sobre o lançamento</p>
             <button type="button" onClick={() => setIsOpen(true)}>
               Inscreva-se
             </button>
           </div>
-          <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
 
+          <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
           <img className="rocket" src={Rocket} alt="foguete" />
         </div>
       </body>
